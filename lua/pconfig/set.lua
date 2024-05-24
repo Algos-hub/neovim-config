63811b6af0170af74f2ef4e1d78bec2f583831d7
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2024-05-24 22:06:12 +0200

    Added highlight on yank

--- lua/pconfig/set.lua
+++ lua/pconfig/set.lua
@@ -1,5 +1,12 @@
 vim.opt.relativenumber = true
 
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Search", timeout = 200 })
+	end,
+})
+
 vim.opt.guicursor = ""
 
 vim.opt.tabstop = 4
