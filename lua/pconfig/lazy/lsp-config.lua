d33fe1e70839c9701bebfbbcaa32d5fcb55460ee
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2024-05-29 09:30:22 +0200

    Added LSP diagnostics to Telescope with the '<leader>fd' keymap

--- lua/pconfig/lazy/lsp-config.lua
+++ lua/pconfig/lazy/lsp-config.lua
@@ -1,138 +1,138 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
 
-		config = function()
-			vim.diagnostic.config({
-				signs = false,
-				underline = true,
-			})
-			-- Overriding the default color scheme given by solarized.nvim
-			local set_hl_for_floating_window = function()
-				vim.api.nvim_set_hl(0, "NormalFloat", {
-					link = "Normal",
-				})
-				vim.api.nvim_set_hl(0, "FloatBorder", {
-					bg = "none",
-				})
-			end
+        config = function()
+            vim.diagnostic.config({
+                signs = false,
+                underline = true,
+            })
+            -- Overriding the default color scheme given by solarized.nvim
+            local set_hl_for_floating_window = function()
+                vim.api.nvim_set_hl(0, "NormalFloat", {
+                    link = "Normal",
+                })
+                vim.api.nvim_set_hl(0, "FloatBorder", {
+                    bg = "none",
+                })
+            end
 
-			set_hl_for_floating_window()
+            set_hl_for_floating_window()
 
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				pattern = "*",
-				desc = "Avoid overwritten by loading color schemes later",
-				callback = set_hl_for_floating_window,
-			})
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            vim.api.nvim_create_autocmd("ColorScheme", {
+                pattern = "*",
+                desc = "Avoid overwritten by loading color schemes later",
+                callback = set_hl_for_floating_window,
+            })
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			-- Add rounded border on hover and on signature help popup window
-			local handlers = {
-				["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-				["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-			}
+            -- Add rounded border on hover and on signature help popup window
+            local handlers = {
+                ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+                ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+            }
 
-			-- Add rounded border to the diagnostic popup window
-			vim.diagnostic.config({
-				virtual_text = {
-					prefix = "■", -- Could be '●', '▎', 'x', '■', , 
-				},
-			})
+            -- Add rounded border to the diagnostic popup window
+            vim.diagnostic.config({
+                virtual_text = {
+                    prefix = "■", -- Could be '●', '▎', 'x', '■', , 
+                },
+            })
 
-			-- Set up lspconfig.
-			require("mason").setup()
+            -- Set up lspconfig.
+            require("mason").setup()
 
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"tsserver",
-					"yamlls",
-					"sqlls",
-					"phpactor",
-					"pylsp",
-					"kotlin_language_server",
-					"jsonls",
-					"html",
-					"eslint",
-					"cssmodules_ls",
-					"cssls",
-					"clangd",
-					"angularls",
-					"vimls",
-					"lua_ls",
-				},
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "tsserver",
+                    "yamlls",
+                    "sqlls",
+                    "phpactor",
+                    "pylsp",
+                    "kotlin_language_server",
+                    "jsonls",
+                    "html",
+                    "eslint",
+                    "cssmodules_ls",
+                    "cssls",
+                    "clangd",
+                    "angularls",
+                    "vimls",
+                    "lua_ls",
+                },
 
-				handlers = {
-					function(server_name)
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-							handlers = handlers,
-						})
-					end,
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							handlers = handlers,
-							settings = {
-								Lua = {
-									runtime = { version = "Lua 5.1" },
-									diagnostics = {
-										globals = { "vim", "it", "describe", "before_each", "after_each" },
-									},
-								},
-							},
-						})
-					end,
-				},
-			})
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities,
+                            handlers = handlers,
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            handlers = handlers,
+                            settings = {
+                                Lua = {
+                                    runtime = { version = "Lua 5.1" },
+                                    diagnostics = {
+                                        globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                    },
+                                },
+                            },
+                        })
+                    end,
+                },
+            })
 
-			vim.api.nvim_create_autocmd("LspAttach", {
+            vim.api.nvim_create_autocmd("LspAttach", {
 
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 
-				callback = function(ev)
-					local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                callback = function(ev)
+                    local client = vim.lsp.get_client_by_id(ev.data.client_id)
 
-					client.server_capabilities.semanticTokensProvider = nil
+                    client.server_capabilities.semanticTokensProvider = nil
 
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<C-m>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "<leader>F", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-		end,
-	},
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "<C-m>", vim.lsp.buf.signature_help, opts)
+                    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wl", function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
+                    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "<leader>F", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, opts)
+                end,
+            })
+        end,
+    },
 }
--- lua/pconfig/lazy/telescope.lua
+++ lua/pconfig/lazy/telescope.lua
@@ -1,37 +1,48 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.6",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-			},
-		},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.6",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+            },
+        },
 
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-			vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopeTitle", { bg = "none" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            vim.keymap.set("n", "<leader>fd", function()
+                builtin.diagnostics({ bufnr = 0 })
+            end, {})
+            vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
+            vim.api.nvim_set_hl(0, "TelescopeTitle", { bg = "none" })
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            local actions = require("telescope.actions")
+            require("telescope").setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<esc>"] = actions.close,
+                        },
+                    },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
